d931c18006361fcbcc289086195a07d667ca441d
baker6romeo <baker6romeo@gmail.com>
2026-04-21 10:07:57 -0700

    Separate out components into different files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
+import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
-import reactImg from "./assets/react-core-concepts.png";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -18,36 +19,6 @@ function App() {
       </main>
     </div>
   );
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title}/>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {descriptionModifier} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-const descriptionModifier = reactDescriptions[getRandomInt(2)];
-
-function getRandomInt (max) {
-   return Math.floor(Math.random() * (max + 1))
 }
 
 export default App;
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {descriptionModifier} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+const descriptionModifier = reactDescriptions[getRandomInt(2)];
+
+function getRandomInt (max) {
+   return Math.floor(Math.random() * (max + 1))
+}
