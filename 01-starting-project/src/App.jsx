49d8f741c9b2b6d6f28874bd1b2eee5edc08f95b
baker6romeo <baker6romeo@gmail.com>
2026-04-24 10:02:32 -0700

    Dynamically populating details window

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,15 +1,16 @@
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [description, setDescription] = useState(EXAMPLES.components.description);
+  const [code, setCode] = useState(EXAMPLES.components.code);
 
   function handleSelect (selectedButton) {
-    console.log(selectedButton);
     setSelectedTopic(selectedButton);
   }
 
@@ -34,9 +35,15 @@ function App() {
               <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
               <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
-
-            {selectedTopic}
-
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
           </section>
       </main>
     </div>
