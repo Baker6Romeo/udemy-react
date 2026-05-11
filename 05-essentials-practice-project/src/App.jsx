9aa5442dbef0eab5aeba1da214510512e28727d8
baker6romeo <baker6romeo@gmail.com>
2026-05-11 12:38:10 -0700

    User input initial creation

--- 05-essentials-practice-project/src/App.jsx
+++ 05-essentials-practice-project/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <UserInput />
+    </main>
   )
 }
 
