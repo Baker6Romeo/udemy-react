0aa7ebe5603340d43c917c98499cfe52fe72a353
baker6romeo <baker6romeo@gmail.com>
2026-05-13 12:56:57 -0700

    Pass input info from UserInput to App. Small modifications to Header component.

--- 05-essentials-practice-project/src/App.jsx
+++ 05-essentials-practice-project/src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import ResultsTable from "./components/ResultsTable";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [inputs, setInputs] = useState({});
+
+  function handleValueChange (name, value) {
+    console.log("Starting Inputs: ", inputs);
+    const updatedInputs = {...inputs, [name]: value}
+    console.log("updatedInputs: ", updatedInputs);
+    setInputs(updatedInputs);
+    console.log("New Inputs value: ", inputs);
+    
+  }
+
+  console.log(inputs)
+
   return (
     <main>
       <Header />
-      <UserInput />
+      <UserInput onChangeValue={handleValueChange}/>
       <ResultsTable />
     </main>
   )
--- 05-essentials-practice-project/src/components/UserInput.jsx
+++ 05-essentials-practice-project/src/components/UserInput.jsx
@@ -1,7 +1,44 @@
-export default function UserInput () {
+export default function UserInput ({onChangeValue}) {
+
+  function handleChange (event) {
+    onChangeValue(event.target.name, event.target.value);
+  }
+
   return (
-    <div id="user-input">
-      <p>user input</p>
-    </div>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+        <label>Initial Investment</label>
+        <input
+          name="initialInvestment"
+          onChange={handleChange}
+          type="number"
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            name="annualInvestment"
+            onChange={handleChange}
+            type="number"/>
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            name="expectedReturn"
+            onChange={handleChange}
+            type="number"/>
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            name="duration"
+            onChange={handleChange}
+            type="number"/>
+        </p>
+      </div>
+    </section>
   );
 }
